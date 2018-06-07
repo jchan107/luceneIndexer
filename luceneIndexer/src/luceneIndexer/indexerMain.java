3e262a5b3559f6ef15908259e1edeec57a7c4225
jchan107 <jchan107@ucr.edu>
2018-06-07 08:41:35 -0700

    Extra checks on urls

--- luceneIndexer/src/luceneIndexer/indexerMain.java
+++ luceneIndexer/src/luceneIndexer/indexerMain.java
@@ -47,79 +47,79 @@ public class indexerMain {
         String basePath = "D:\\index";
         Directory directory = FSDirectory.open(Paths.get(basePath));
         
-//        IndexWriterConfig config = new IndexWriterConfig(analyzer);
-//		IndexWriter w = new IndexWriter(directory, config);
-//		try
-//		{
-//			File txt = new File(args[0] + "\\\\index.txt");
-//			FileReader fileReader = new FileReader(txt);
-//			BufferedReader bufferedReader = new BufferedReader(fileReader);
-//			String line;
-//			int cnt = 0;// just to check indexing on smaller number of htmls
-////			for(int i = 0; i < 1;i++)
-////			{
-////				line = bufferedReader.readLine();
-////			}
-//			while ((line = bufferedReader.readLine()) != null) {
-//				String[] indexANDUrl = line.split("\\|");
-//				String index = indexANDUrl[0];
-//				String url = indexANDUrl[1];
-//				
-//				if(url.contains("www"))
-//				{
-//					url = url.replace("www.", "");
-//				}
-//				
-//				//strips the '#' symbol and any following text from url if it exists
-//				if(url.contains("#"))
-//				{
-//					String[] onlyUrl = url.split("\\#");
-//					url = onlyUrl[0];
-//				}
-//				
-//				//strips the '/' character from url if it exists
-//				if(url.charAt(url.length()-1) == '/')
-//				{
-//					url = url.substring(0, url.length()-1);
-//				}
-//				
-//				//Checks if the path leads to a pdf
-//				if(url.substring(url.length()-4,url.length()).equals(".pdf"))
-//				{
-//					continue;
-//				}
-//				
-//				
-//				System.out.println(index);
-//				System.out.println(url);
-//				File htmlF = new File(args[0] + "\\" + index + ".html");
-//				org.jsoup.nodes.Document doc = Jsoup.parse(htmlF, "UTF-8", "");
-//				String title = doc.getElementsByTag("title").text();
-//				System.out.println(title);
-//				String body = doc.getElementsByTag("body").text();
-////				System.out.println(body);
-////				System.out.println();
-//				
-//				//
-//				if(!addedUrls.contains(url))
-//				{
-//					Document d = getDocument(title,body,url);
-//					w.addDocument(d);
-//					addedUrls.add(url);
-//					cnt++;
-//				}
-//				else
-//				{
-//					System.out.println("DUPLICATE");
-//				}
+        IndexWriterConfig config = new IndexWriterConfig(analyzer);
+		IndexWriter w = new IndexWriter(directory, config);
+		try
+		{
+			File txt = new File(args[0] + "\\\\index.txt");
+			FileReader fileReader = new FileReader(txt);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			String line;
+			int cnt = 0;// just to check indexing on smaller number of htmls
+//			for(int i = 0; i < 1;i++)
+//			{
+//				line = bufferedReader.readLine();
 //			}
-//			bufferedReader.close();
-//			w.close();
-//			System.out.println(cnt);
-//		}
-//		catch (IOException e) {
-//			e.printStackTrace();
-//		}
+			while ((line = bufferedReader.readLine()) != null) {
+				String[] indexANDUrl = line.split("\\|");
+				String index = indexANDUrl[0];
+				String url = indexANDUrl[1];
+				
+				if(url.contains("www"))
+				{
+					url = url.replace("www.", "");
+				}
+				
+				//strips the '#' symbol and any following text from url if it exists
+				if(url.contains("#"))
+				{
+					String[] onlyUrl = url.split("\\#");
+					url = onlyUrl[0];
+				}
+				
+				//strips the '/' character from url if it exists
+				if(url.charAt(url.length()-1) == '/')
+				{
+					url = url.substring(0, url.length()-1);
+				}
+				
+				//Checks if the path leads to a pdf
+				if(url.substring(url.length()-4,url.length()).equals(".pdf"))
+				{
+					continue;
+				}
+				
+				
+				System.out.println(index);
+				System.out.println(url);
+				File htmlF = new File(args[0] + "\\" + index + ".html");
+				org.jsoup.nodes.Document doc = Jsoup.parse(htmlF, "UTF-8", "");
+				String title = doc.getElementsByTag("title").text();
+				System.out.println(title);
+				String body = doc.getElementsByTag("body").text();
+//				System.out.println(body);
+//				System.out.println();
+				
+				//
+				if(!addedUrls.contains(url))
+				{
+					Document d = getDocument(title,body,url);
+					w.addDocument(d);
+					addedUrls.add(url);
+					cnt++;
+				}
+				else
+				{
+					System.out.println("DUPLICATE");
+				}
+			}
+			bufferedReader.close();
+			w.close();
+			System.out.println(cnt);
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 		
 		//search Index
@@ -142,25 +142,24 @@ public class indexerMain {
             Document hitDoc = indexSearcher.doc(hits[rank].doc);
             	System.out.println((rank + 1) + " (score:" + hits[rank].score + ") --> " +
                         hitDoc.get("title") + " - " + hitDoc.get("url") + " - " + hitDoc.get("body"));
-            // System.out.println(indexSearcher.explain(query, hits[rank].doc));
         }
         indexReader.close();
         directory.close();
     }
 
-	static IndexWriter getIndexWriter(String dir) {
-		Directory indexDir;
-		try {
-			indexDir = FSDirectory.open(Paths.get("builtIndex"));
-			IndexWriterConfig luceneConfig = new IndexWriterConfig(
-					new StandardAnalyzer());
-			return(new IndexWriter(indexDir, luceneConfig));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return null;
-	}
+//	static IndexWriter getIndexWriter(String dir) {
+//		Directory indexDir;
+//		try {
+//			indexDir = FSDirectory.open(Paths.get("builtIndex"));
+//			IndexWriterConfig luceneConfig = new IndexWriterConfig(
+//					new StandardAnalyzer());
+//			return(new IndexWriter(indexDir, luceneConfig));
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		return null;
+//	}
 	
 	protected static Document getDocument(String title, String body,String url) {
 		Document doc = new Document();
